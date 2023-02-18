a951f911a2bc234de2f72553be425fcdbba3bd24
Pablo Boing Pscheidt <nikebryt@gmail.com>
2023-02-17 22:27:27 -0300

    Finalização do css primário e começo de script

--- web/scripts/script.js
+++ web/scripts/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     // script para o checkbox personalizado realizar a função do check nativo.
+    $('.carga-hor .checkbox-styled input[checked]').closest('.checkbox-styled').addClass('checked'); 
+
     $('.carga-hor .checkbox-styled').on('click', function() {
         $(this).next('label').click();
         $('.carga-hor .checkbox-styled').removeClass('checked');
@@ -12,4 +14,23 @@ $(document).ready(function() {
         $(this).next('label').click();
         $(this).toggleClass('checked');
     })
+
+    $('.banco-horas .checkbox-banco .checkbox-styled').on('click', function() {
+        $(this).next('label').click();
+        $(this).toggleClass('checked');
+        if ($(this).hasClass('checked')) {
+            $('.meu-banco-horas').animate({ "height": "150" }, "slow" )
+        } else {
+            $('.meu-banco-horas').animate({ "height": "0" }, "slow" )
+        }
+    })
+
+    // Inputmask.extendDefinitions({
+    //     '00': {
+    //         validator: "/[0-23]/",
+    //     },
+    //     '11': {
+    //         validator: "/[0-59]/",
+    //     }
+    // });
 });
